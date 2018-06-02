23f42db215f107bf5ca45f42691b574ed7a0fe9f
Numa Navarro <lfnna1977@yahoo.es>
2018-06-02 16:18:55 -0400

    Añadiendo limites a la prueba

--- src/test/java/PersistTest.java
+++ src/test/java/PersistTest.java
@@ -9,5 +9,9 @@ public class PersistTest {
         // Se introduce el numero que se quiere utilizar para hacer las pruebas
         int result = Persist.persistence(39);
         assertEquals(3, result);
+        result = Persist.persistence(4);
+        assertEquals(0, result);
+        result = Persist.persistence(1000);
+        assertEquals(1, result);
     }
 }
